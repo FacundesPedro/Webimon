e28b19310dff143490fc0736746bc6c1c82d30c0
FacundesPedro <pedro.f.facundes1@gmail.com>
2022-02-23 19:41:45 -0300

    Added char classes

--- src/Components/char/Char.jsx
+++ src/Components/char/Char.jsx
@@ -3,10 +3,12 @@ import './char.css'
 
 const Char = () =>{
     const [imageUrl, setimageUrl] = useState("https://s3-us-west-2.amazonaws.com/s.cdpn.io/21542/DemoRpgCharacter.png")
+    const [shadowUrl, setshadowUrl] = useState("https://s3-us-west-2.amazonaws.com/s.cdpn.io/21542/DemoRpgCharacterShadow.png")
     
     return (
     <div className="charContainer">
-        <img className='charSpriteSheet pixelated' src={imageUrl} />
+        <img className='charShadow pixelated' src={shadowUrl} alt="Default Shadow"  />
+        <img className='charSpriteSheet pixelated ' alt='Default Sprite' src={imageUrl} />
     </div>
     )
 }
